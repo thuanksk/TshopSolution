ec5c2a3c928474005a74901c19d31c7ca1daad3a
thuận nguyễn <nguyenvanthuan23693@gmail.com>
2020-11-11 23:35:13 +0700

    create database

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 #ASP.NET core 3.1 project form Tshop
+-- Asp.net core 3.1
+- Entity frameword core 3.1
+## instal packages
+- Miscrosoft.EntityFrameworkCore.Sqlserver
+- Miscrosoft.EntityFrameworkCore.Tools
+- Miscrosoft.EntityFameworkCore.Design
+## create database 
+- 13 table 
+- 3 class enum
---
+++ TshopSolution.Data/EF/TshopDBcontext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TshopSolution.Data.Entites;
+
+namespace TshopSolution.Data.EF
+{
+    public class TshopDBcontext : DbContext
+    {
+        public TshopDBcontext( DbContextOptions options) : base(options)
+        {
+           
+        }
+
+
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
