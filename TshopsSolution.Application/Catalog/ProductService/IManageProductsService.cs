5a731c9a058872533e447062fe6df5118e96e79e
thuận nguyễn <nguyenvanthuan23693@gmail.com>
2020-12-14 21:57:56 +0700

    create struct of application_layer

---
+++ TshopsSolution.Application/Catalog/ProductService/IManageProductsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TshopsSolution.Application.Catalog.ProductService.Dtos;
+using TshopsSolution.Application.Dtos;
+
+namespace TshopsSolution.Application.Catalog.ProductService
+{
+    public interface IManageProductsService
+    {
+        Task<int> Create(ProductCreateRequest request);
+        Task<int> Update(ProductEditRequest request);
+        Task<int> Delete(int productId);
+        Task<List<ProductViewModel>> GetAll();
+        Task<PageViewModel<ProductViewModel>> GetAllPaging(string keywork, int pageIndex, int pageSize);
+
+    }
+}
---
+++ TshopsSolution.Application/Catalog/ProductService/IPublicProducService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TshopsSolution.Application.Catalog.ProductService.Dtos;
+using TshopsSolution.Application.Dtos;
+
+namespace TshopsSolution.Application.Catalog.ProductService
+{
+    public interface IPublicProducService
+    {
+        PageViewModel<ProductViewModel> GetAllByCategoryId(int categoryID, int pageIndex, int pageSize);
+
+    }
+}
